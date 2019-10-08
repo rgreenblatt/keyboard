15084bcff0cbc927552d3ccf4fc279f71515eee9
rgreenblatt <greenblattryan@gmail.com>
2019-10-08 16:42:06 -0400

    Bug fixes

--- src/notify.py
+++ src/notify.py
@@ -2,16 +2,16 @@ import os
 from subprocess import check_output
 from time import sleep
 
-from keyboard.layers import MyLayerHandler
+from keyboard.layers import files
 from keyboard.utils import alert
 
-if __name__ == "__main__":
+def main():
     prev_capslock_on = False
     prev_sym_lock_on = False
-    while (True):
+    while True:
         capslock_on = check_output("check_caps_on").decode(
             "utf-8").strip() == "on"
-        sym_lock_on = os.path.isfile(MyLayerHandler.files["sym_toggle"])
+        sym_lock_on = os.path.isfile(files["sym_toggle"])
         if capslock_on and not prev_capslock_on:
             alert("CAPS LOCK", time=1)
         if sym_lock_on and not prev_sym_lock_on:
@@ -19,3 +19,6 @@ if __name__ == "__main__":
         prev_capslock_on = capslock_on
         prev_sym_lock_on = sym_lock_on
         sleep(0.1)
+
+if __name__ == "__main__":
+    main()
